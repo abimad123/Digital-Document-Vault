3bb8f4306ccc2b7914bc439e0b68e9784e048fea
Abijith CG <abijithc140@gmail.com>
2026-02-12 21:34:23 +0530

    add the backend folders , add the user , db server logics

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: [true, 'Authorized name required'],
+    trim: true 
+  },
+  email: { 
+    type: String, 
+    required: [true, 'Secure email required'], 
+    unique: true,
+    lowercase: true
+  },
+  password: { 
+    type: String, 
+    required: [true, 'Cryptographic key required'],
+    minlength: 8,
+    select: false 
+  },
+  tier: { 
+    type: String, 
+    enum: ['Personal', 'Pro', 'Enterprise'], 
+    default: 'Personal' // Default free plan
+  },
+  
+  billing: {
+    cardNumber: { type: String, select: false },
+    expiry: { type: String, select: false },
+    cvc: { type: String, select: false },
+    isPaid: { type: Boolean, default: false }
+  },
+
+  // --- VERIFICATION PROTOCOL ---
+  isVerified: { type: Boolean, default: false },
+  verificationToken: String,
+  verificationTokenExpire: Date
+}, { 
+  timestamps: true 
+});
+
+module.exports = mongoose.model('User', UserSchema);
